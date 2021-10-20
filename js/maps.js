db1c1d63d89a0916fe08533312a5f6a3217f6297
Mel Watts <mel.watts@therooflightcompany.co.uk>
2021-10-20 15:24:00 +0000

    Create a meet the trainers page

---
+++ js/maps.js
@@ -0,0 +1,18 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    var labels = "AIzaSyBoj3Qd7B3WIEseB7KD5bSs7HaQ9rUBoFw";
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+}
