cfb606b8bf29f2756857fdbbe40e49488f603c08
Mel Watts <mel.watts@therooflightcompany.co.uk>
2021-10-29 15:03:16 +0000

    Add new pages for the classes and add hero images for each

--- js/maps.js
+++ js/maps.js
@@ -1,18 +1,9 @@
-function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: {
-            lat: 46.619261,
-            lng: -33.134766
-        }
-    });
-
-    var labels = "AIzaSyDVtuPoUye4BAmEca2gLEJRlqfDScyGs3U";
-
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 51.752022, lng:  -1.257677},
+          zoom: 8
         });
-    });
+     
+    var labels = "AIzaSyDVtuPoUye4BAmEca2gLEJRlqfDScyGs3U";
 }
