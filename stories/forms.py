8c3810d98eecd19f986c9a7cd53e67c17d340117
Mel Watts <mel.watts@therooflightcompany.co.uk>
2022-04-03 11:36:24 +0000

    add widget info for uplaod user story

--- stories/forms.py
+++ stories/forms.py
@@ -1,26 +1,27 @@
-# from django import forms
-# from .models import Stories, Comment
+from django import forms
+# from .widgets import CustomClearableFileInput
+from .models import Stories, Comment
 
 
-# class StoryForm(forms.ModelForm):
-#     class Meta:
-#         model = Stories
-#         fields = '__all__'
-    
-#     image = forms.ImageField(label='Image',
-#                         required=False
+class StoryForm(forms.ModelForm):
+    class Meta:
+        model = Stories
+        fields = '__all__'
 
+    image_before = forms.ImageField(label='Image',
+                        required=False)
+                        # widget=CustomClearableFileInput)
 
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         categories = Category.objects.all()
-#         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
-#         self.fields['category'].choices = friendly_names
-#         for field_name, field in self.fields.items():
-#             field.widget.attrs['class'] = 'border-black rounded-0'
-    
-# class CommentForm(forms.ModelForm):
-#     class Meta:
-#         model = Comment
-#         fields = ('name', 'email', 'body',)
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body',)
