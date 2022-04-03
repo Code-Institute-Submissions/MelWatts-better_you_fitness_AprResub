9758b039549115db8c041246c7e97d48705acc9e
Mel Watts <mel.watts@therooflightcompany.co.uk>
2022-04-03 12:19:42 +0000

    Fix bug in image no showing by run migration

--- stories/models.py
+++ stories/models.py
@@ -8,7 +8,9 @@ class Stories(models.Model):
 
     name = models.CharField(max_length=254)
     description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image_before = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image_after = models.ImageField(null=True, blank=True)
 
     def __str__(self):
