cecce7778980a5208757d67824aef17759c69ba6
Mel Watts <mel.watts@therooflightcompany.co.uk>
2022-04-07 11:10:43 +0000

    Add edit story template

--- stories/views.py
+++ stories/views.py
@@ -69,7 +69,8 @@ def add_stories(request):
         form = StoryForm(request.POST, request.FILES)
         if form.is_valid():
             story = form.save()
-            messages.success(request, 'Great! Thanks for the Story!')
+            jls_extract_var = 'Great! Thanks for you story,!'
+            messages.success(request, jls_extract_var)
             return redirect(reverse('story_detail', args=[story.id]))
         else:
             messages.error(request, 'Opps! Please check your form is valid.')
